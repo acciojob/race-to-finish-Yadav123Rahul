82d3c6c18a8ba066793cc2c8c1a6c41eb5de5aad
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-08-29 12:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+
 window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTime(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function createRandomPromise() {
+	const randomTime = getRandomTime(1000, 5000);
+    return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(`Promise resolved after ${randomTime}ms`);
+		}, randomTime);
+	});
+}
+
+for (let i = 0; i < 5; i++) {
+	promises.push(createRandomPromise());
+}
+Promise.any(promises)
+	.then((result) => {
+		const div = document.getElementById("output");
+		div.textContent = result;
+	})
+	.catch((error) => {
+		console.error("All promises rejected:", error);
+	});
